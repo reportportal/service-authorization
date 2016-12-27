928530300fc491b905469a449c183a2471620236
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2016-12-27 14:51:35 +0300

    add dynamic github oauth

--- src/main/java/com/epam/reportportal/auth/DemoDataEvent.java
+++ src/main/java/com/epam/reportportal/auth/DemoDataEvent.java
@@ -17,6 +17,8 @@ import java.util.Optional;
  * Created by andrei_varabyeu on 12/26/16.
  */
 @Component
+@Deprecated
+//TODO do not forget to remove!
 public class DemoDataEvent {
 
 	@Autowired
@@ -26,25 +28,6 @@ public class DemoDataEvent {
 	public void onStart(){
 		ServerSettings settings = serverSettingsRepository.findOne("default");
 		settings.setoAuth2LoginDetails(null);
-
-//		Optional<OAuth2LoginDetails> githubDetails = Optional.ofNullable(settings.getoAuth2LoginDetails())
-//				.flatMap(details -> details.stream().filter(d -> "github".equals(d.getId())).findAny());
-//		if (!githubDetails.isPresent()){
-//			OAuth2LoginDetails oAuth2LoginDetails = new OAuth2LoginDetails();
-//			oAuth2LoginDetails.setScope(Collections.singletonList("user"));
-//			oAuth2LoginDetails.setId("github");
-//			oAuth2LoginDetails.setGrantType("authorization_code");
-//			oAuth2LoginDetails.setClientId("f4cec43d4541283879c4");
-//			oAuth2LoginDetails.setClientSecret("a31aa6de3e27c11d90762cad11936727d6b0759e");
-//			oAuth2LoginDetails.setAccessTokenUri("https://github.com/login/oauth/access_token");
-//			oAuth2LoginDetails.setUserAuthorizationUri("https://github.com/login/oauth/authorize");
-//
-//			List<OAuth2LoginDetails> detailsList = Optional.ofNullable(settings.getoAuth2LoginDetails()).orElseGet(ArrayList::new);
-//			detailsList.add(oAuth2LoginDetails);
-//
-//			settings.setoAuth2LoginDetails(detailsList);
-//
-//		}
 
 
 		OAuth2LoginDetails oAuth2LoginDetails = new OAuth2LoginDetails();
