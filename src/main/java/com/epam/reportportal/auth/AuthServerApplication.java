561c9cdfb8cc5de1cb8db37e9370cf9bd7a165c9
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-01-17 17:06:20 +0300

    enable mongo session

--- src/main/java/com/epam/reportportal/auth/AuthServerApplication.java
+++ src/main/java/com/epam/reportportal/auth/AuthServerApplication.java
@@ -37,6 +37,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -66,6 +67,7 @@ public class AuthServerApplication {
 	 * execution
 	 */
 	@Configuration
+	@EnableMongoHttpSession
 	public static class MvcConfig extends WebMvcConfigurerAdapter {
 
 		@Autowired
