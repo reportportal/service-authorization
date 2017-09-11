369ee814ea4b98a650af60ee824fb1d7a36042a0
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-09-11 13:26:40 +0300

    put project roles into the /sso/me endpoint

--- src/main/java/com/epam/reportportal/auth/DatabaseUserDetailsService.java
+++ src/main/java/com/epam/reportportal/auth/DatabaseUserDetailsService.java
@@ -20,17 +20,14 @@
  */
 package com.epam.reportportal.auth;
 
-import com.epam.ta.reportportal.database.dao.ProjectRepository;
 import com.epam.ta.reportportal.database.dao.UserRepository;
-import com.epam.ta.reportportal.database.entity.Project;
-import com.epam.ta.reportportal.database.entity.ProjectRole;
+import com.epam.ta.reportportal.database.entity.UserRoleDetails;
 import com.epam.ta.reportportal.database.entity.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -44,24 +41,20 @@ class DatabaseUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 
-	@Autowired
-	private ProjectRepository projectRepository;
-
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User userEntity = userRepository.findOne(username.toLowerCase());
-		if (null == userEntity) {
+		UserRoleDetails userEntity = userRepository.aggregateUserProjects(username.toLowerCase());
+		if (null == userEntity || null == userEntity.getUser()) {
 			throw new UsernameNotFoundException("Username '" + username + "' not found");
 		}
 
-		String login = userEntity.getLogin();
-		String password = userEntity.getPassword() == null ? "" : userEntity.getPassword();
+		String login = userEntity.getUser().getLogin();
+		String password = userEntity.getUser().getPassword() == null ? "" : userEntity.getUser().getPassword();
 
-		Map<String, ProjectRole> projectRoles = projectRepository.findUserProjects(login).stream()
-				.collect(Collectors.toMap(Project::getId, p -> p.getUsers().get(login).getProjectRole()));
 
 		org.springframework.security.core.userdetails.User u = new org.springframework.security.core.userdetails.User(login, password, true,
-				true, true, true, AuthUtils.AS_AUTHORITIES.apply(userEntity.getRole()));
-		return new ReportPortalUser(u, projectRoles);
+				true, true, true, AuthUtils.AS_AUTHORITIES.apply(userEntity.getUser().getRole()));
+		return new ReportPortalUser(u, userEntity.getProjects().stream().collect(Collectors.toMap(UserRoleDetails.ProjectDetails::getProject,
+				UserRoleDetails.ProjectDetails::getProjectRole)));
 	}
 }
