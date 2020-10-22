97ae779586b0beedd3f51b0ced67a836b760f9aa
pavel_bortnik <pavel_bortnik@epam.com>
2020-10-23 01:03:03 +0300

    Fix saml integration validation

--- src/main/java/com/epam/reportportal/auth/integration/handler/impl/CreateOrUpdateSamlIntegrationStrategy.java
+++ src/main/java/com/epam/reportportal/auth/integration/handler/impl/CreateOrUpdateSamlIntegrationStrategy.java
@@ -72,7 +72,7 @@ public class CreateOrUpdateSamlIntegrationStrategy extends CreateOrUpdateIntegra
 	@Override
 	protected void beforeCreate(UpdateAuthRQ request) {
 		ParameterUtils.validateSamlRequest(request);
-		if (FULL_NAME_ATTRIBUTE.getParameter(request).isEmpty() || (LAST_NAME_ATTRIBUTE.getParameter(request).isEmpty()
+		if (FULL_NAME_ATTRIBUTE.getParameter(request).isEmpty() && (LAST_NAME_ATTRIBUTE.getParameter(request).isEmpty()
 				&& FIRST_NAME_ATTRIBUTE.getParameter(request).isEmpty())) {
 			throw new ReportPortalException(ErrorType.BAD_REQUEST_ERROR,
 					"Fields Full name or combination of Last name and First name are empty"
