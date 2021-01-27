36b3139bbc47d796e07b93cc51459637e605efb5
Pavel_Bortnik <pavel_bortnik@epam.com>
2021-01-27 14:57:53 +0300

    Add search filter field to ad integration

--- src/main/java/com/epam/reportportal/auth/integration/converter/ActiveDirectoryConverter.java
+++ src/main/java/com/epam/reportportal/auth/integration/converter/ActiveDirectoryConverter.java
@@ -34,6 +34,7 @@ public final class ActiveDirectoryConverter {
 		ActiveDirectoryResource resource = new ActiveDirectoryResource();
 		resource.setId(adIntegration.getId());
 		LdapParameter.DOMAIN.getParameter(adIntegration).ifPresent(resource::setDomain);
+		LdapParameter.SEARCH_FILTER.getParameter(adIntegration).ifPresent(resource::setSearchFilter);
 		resource.setLdapAttributes(LdapConverter.LDAP_ATTRIBUTES_TO_RESOURCE.apply(adIntegration));
 		return resource;
 	};
