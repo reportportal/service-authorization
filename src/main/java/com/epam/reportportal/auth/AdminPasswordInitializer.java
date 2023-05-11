ee984339772b8c224f46f388325dbdb3cdf5be33
APiankouski <109206864+APiankouski@users.noreply.github.com>
2023-05-11 14:07:06 +0300

    Release 23.1
    * EPMRPP-80606 || Update item attribute key and value validation (#233)
    
    EPMRPP-80606 || Update item attribute key and value validation
    
    * EPMRPP-81360 || Fix vulnerabilities
    
    * EPMRPP-82514 || Email sync doesn't work for existing LDAP users (#247)
    
    * EPMRPP-82514 added email update and legacy code refactoring
    
    * EPMRPP-82514 remove old comment
    
    * EPMRPP-82549 added AdminPasswordInitializer for changing default password
    
    * Update commons dao
    
    * Merge master to 5.7.5 (#252)
    
    * EPMRPP-79211 || Changed Minio interface to JCloud
    
    * EPMRPP-79211 || Updated DataStore Configuration and properties
    
    * EPMRPP-79211 || Updated commons-dao dependency
    
    * Update commons dao dependency to 5.7.4
    
    * Update release.yml
    
    * EPMRPP-80865|| Update bom and other versions (#241)
    
    5.7.4 || Update bom and other versions
    
    * [Gradle Release Plugin] - new version commit:  '5.7.5'.
    
    * EPMRPP-82673-exec-jar promote.yml update (added exec jar)
    
    ---------
    
    Co-authored-by: Maksim Antonov <Maksim_Antonov@epam.com>
    Co-authored-by: miracle8484 <76156909+miracle8484@users.noreply.github.com>
    Co-authored-by: reportportal.io <support@reportportal.io>
    Co-authored-by: rkukharenka <ryhor_kukharenka@epam.com>
    Co-authored-by: Ryhor <125865748+rkukharenka@users.noreply.github.com>
    
    * Update version
    
    * EPMRPP-80498 || Expose saml leve session parameter
    
    * EPMRPP-83246 || Superadmin's password is reset everytime when updating ReportPortal
    
    * EPMRPP-83246 || Superadmin's password is reset everytime when updating ReportPortal. Fix Value
    
    * Update release version
    
    ---------
    
    Co-authored-by: Ivan Kustau <86599591+IvanKustau@users.noreply.github.com>
    Co-authored-by: Maksim Antonov <Maksim_Antonov@epam.com>
    Co-authored-by: miracle8484 <76156909+miracle8484@users.noreply.github.com>
    Co-authored-by: Ryhor <125865748+rkukharenka@users.noreply.github.com>
    Co-authored-by: rkukharenka <ryhor_kukharenka@epam.com>
    Co-authored-by: Ivan_Kustau <Ivan_Kustau@epam.com>
    Co-authored-by: reportportal.io <support@reportportal.io>
    Co-authored-by: Andrei Piankouski <andrei_piankouski@epam.com>

---
+++ src/main/java/com/epam/reportportal/auth/AdminPasswordInitializer.java
@@ -0,0 +1,72 @@
+package com.epam.reportportal.auth;
+
+import static java.util.Optional.ofNullable;
+
+import com.epam.reportportal.auth.exception.EnvironmentVariablesNotProvidedException;
+import com.epam.ta.reportportal.dao.UserRepository;
+import com.epam.ta.reportportal.entity.user.User;
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class AdminPasswordInitializer implements CommandLineRunner {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AdminPasswordInitializer.class);
+  private static final String SUPER_ADMIN_LOGIN = "superadmin";
+  private static final String ERROR_MSG = "Password not set in environment variable";
+  public static final String USER_LAST_LOGIN = "last_login";
+  public static final Integer INITIAL_LAST_LOGIN = 0;
+
+  private final UserRepository userRepository;
+
+  @Value("${rp.initial.admin.password}")
+  private String adminPassword;
+
+  public AdminPasswordInitializer(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  @Transactional
+  public void run(String... args) {
+    checkPasswordEnvVariable();
+
+    User user = userRepository.findByLogin(SUPER_ADMIN_LOGIN)
+        .orElseThrow(() -> new EntityNotFoundException(SUPER_ADMIN_LOGIN + " not found"));
+    Optional<Object> lastLogin = ofNullable(user.getMetadata())
+      .flatMap(metadata -> ofNullable(metadata.getMetadata()))
+      .map(meta -> meta.get(USER_LAST_LOGIN))
+      .or(() -> Optional.of(INITIAL_LAST_LOGIN));
+
+    boolean isMatches = passwordEncoder().matches(adminPassword, user.getPassword());
+    if (!isMatches && lastLogin.get().equals(INITIAL_LAST_LOGIN)) {
+      updatePasswordForDefaultAdmin(user);
+    }
+  }
+
+  private void updatePasswordForDefaultAdmin(User defaultAdmin) {
+    defaultAdmin.setPassword(passwordEncoder().encode(adminPassword));
+    userRepository.save(defaultAdmin);
+  }
+
+  private void checkPasswordEnvVariable() {
+    if (StringUtils.isBlank(adminPassword)) {
+      LOGGER.error(ERROR_MSG);
+      throw new EnvironmentVariablesNotProvidedException(ERROR_MSG);
+    }
+  }
+
+  private PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
