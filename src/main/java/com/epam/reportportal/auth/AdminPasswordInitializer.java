efb2731a1655ce3f8d5086014b6c94cd66cb1e62
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2023-11-16 17:51:44 +0300

    EPMRPP-87594 fix CVEs (#288)
    * EPMRPP-87594 fix CVEs

--- src/main/java/com/epam/reportportal/auth/AdminPasswordInitializer.java
+++ src/main/java/com/epam/reportportal/auth/AdminPasswordInitializer.java
@@ -7,7 +7,7 @@ import com.epam.ta.reportportal.dao.UserRepository;
 import com.epam.ta.reportportal.entity.user.User;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
