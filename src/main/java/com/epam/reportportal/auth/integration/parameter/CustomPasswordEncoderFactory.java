d9205314c872566e9d4ef048c8573c14b8044c9f
Andrei Piankouski <andrei_piankouski@epam.com>
2024-08-14 13:22:49 +0300

    EPMRPP-87165 || Add logs

--- src/main/java/com/epam/reportportal/auth/integration/parameter/CustomPasswordEncoderFactory.java
+++ src/main/java/com/epam/reportportal/auth/integration/parameter/CustomPasswordEncoderFactory.java
@@ -19,7 +19,6 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
@@ -34,19 +33,19 @@ public class CustomPasswordEncoderFactory {
     Map<String, PasswordEncoder> encoders = new HashMap<>();
     encoders.put("bcrypt", new BCryptPasswordEncoder());
     encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
-    encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
-    encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
+    encoders.put("md4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
+    encoders.put("md5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
     encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
     encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
     Pbkdf2PasswordEncoder PBKDF2_SHA256 = new Pbkdf2PasswordEncoder();
     PBKDF2_SHA256.setAlgorithm(SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-    encoders.put("PBKDF2_SHA256", PBKDF2_SHA256);
+    encoders.put("pbkdf2_sha256", PBKDF2_SHA256);
     Pbkdf2PasswordEncoder PBKDF2_SHA512 = new Pbkdf2PasswordEncoder();
     PBKDF2_SHA512.setAlgorithm(SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA512);
-    encoders.put("PBKDF2_SHA512", new Pbkdf2PasswordEncoder());
+    encoders.put("pbkdf2_sha512", new Pbkdf2PasswordEncoder());
     encoders.put("scrypt", new SCryptPasswordEncoder());
-    encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
-    encoders.put("SHA-256",
+    encoders.put("sha-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
+    encoders.put("sha-256",
         new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
     encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
     encoders.put("argon2", new Argon2PasswordEncoder());
