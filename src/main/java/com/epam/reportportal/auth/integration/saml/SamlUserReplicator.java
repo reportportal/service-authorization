cd15612f281a170a23d1ee5564a17cd522aaf24b
Reingold Shekhtel <raikbitters@gmail.com>
2025-04-16 18:50:39 +0200

    Refactor SamlUserReplicator to improve user replication logic and enhance email resolution

--- src/main/java/com/epam/reportportal/auth/integration/saml/SamlUserReplicator.java
+++ src/main/java/com/epam/reportportal/auth/integration/saml/SamlUserReplicator.java
@@ -16,7 +16,6 @@
 
 package com.epam.reportportal.auth.integration.saml;
 
-import static com.epam.reportportal.auth.util.AuthUtils.CROP_DOMAIN;
 import static com.epam.reportportal.auth.util.AuthUtils.NORMALIZE_STRING;
 
 import com.epam.reportportal.auth.binary.UserBinaryDataService;
@@ -38,14 +37,14 @@ import com.epam.reportportal.auth.integration.AbstractUserReplicator;
 import com.epam.reportportal.auth.integration.AuthIntegrationType;
 import com.epam.reportportal.auth.integration.parameter.SamlParameter;
 import com.epam.reportportal.auth.model.saml.SamlResponse;
+import com.epam.reportportal.auth.oauth.UserSynchronizationException;
 import com.epam.reportportal.auth.rules.exception.ErrorType;
 import com.epam.reportportal.auth.rules.exception.ReportPortalException;
 import com.epam.reportportal.auth.util.PersonalProjectService;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticationToken;
@@ -55,7 +54,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * Replicates user from SAML response into database if it is not exist.
  *
- * @author <a href="mailto:andrei_piankouski@epam.com">Andrei Piankouski</a>
+ * @author <a href="mailto:Reingold_Shekhtel@epam.com">Reingold Shekhtel</a>
  */
 @Component
 public class SamlUserReplicator extends AbstractUserReplicator {
@@ -65,6 +64,17 @@ public class SamlUserReplicator extends AbstractUserReplicator {
 
   private final ApplicationEventPublisher eventPublisher;
 
+  /**
+   * SAML user replicator constructor.
+   *
+   * @param userRepository            User repository
+   * @param projectRepository         Project repository
+   * @param personalProjectService    Personal project service
+   * @param userBinaryDataService     User binary data service
+   * @param integrationTypeRepository Integration type repository
+   * @param integrationRepository     Integration repository
+   * @param contentTypeResolver       Content type resolver
+   */
   @Autowired
   public SamlUserReplicator(UserRepository userRepository, ProjectRepository projectRepository,
       PersonalProjectService personalProjectService, UserBinaryDataService userBinaryDataService,
@@ -79,6 +89,12 @@ public class SamlUserReplicator extends AbstractUserReplicator {
     this.eventPublisher = eventPublisher;
   }
 
+  /**
+   * Replicates user from SAML response into database if it is not exist.
+   *
+   * @param samlAuthentication SAML authentication token
+   * @return {@link User}
+   */
   @Transactional
   public User replicateUser(Saml2AuthenticationToken samlAuthentication) {
     SamlResponse samlResponse;
@@ -88,48 +104,57 @@ public class SamlUserReplicator extends AbstractUserReplicator {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
-    String userEmail = samlResponse.getNameId().value();
-    Optional<User> userOptional = userRepository.findByEmail(userEmail);
 
-    if (userOptional.isPresent()) {
-      return userOptional.get();
-    }
+    var email = Optional.ofNullable(samlResponse.getNameId().value())
+        .filter(StringUtils::isNotBlank)
+        .map(NORMALIZE_STRING)
+        .orElseThrow(() -> new UserSynchronizationException(
+            "SAML response does not contain email")
+        );
 
-    IntegrationType samlIntegrationType =
-        integrationTypeRepository.findByName(AuthIntegrationType.SAML.getName()).orElseThrow(
-            () -> new ReportPortalException(ErrorType.AUTH_INTEGRATION_NOT_FOUND,
-                AuthIntegrationType.SAML.getName()
-            ));
+    return userRepository.findByEmail(email)
+        .orElseGet(() -> {
+          IntegrationType samlIntegrationType =
+              integrationTypeRepository.findByName(AuthIntegrationType.SAML.getName())
+                  .orElseThrow(() -> new ReportPortalException(
+                      ErrorType.AUTH_INTEGRATION_NOT_FOUND,
+                      AuthIntegrationType.SAML.getName()
+                  ));
 
-    List<Integration> providers = integrationRepository.findAllGlobalByType(samlIntegrationType);
+          Integration provider = integrationRepository.findAllGlobalByType(samlIntegrationType)
+              .stream()
+              .filter(integration -> {
+                var alias = SamlParameter.IDP_ALIAS.getParameter(integration);
+                return alias.isPresent() && alias.get().equalsIgnoreCase(samlResponse.getIssuer());
+              })
+              .findFirst()
+              .orElseThrow(() -> new ReportPortalException(
+                  ErrorType.AUTH_INTEGRATION_NOT_FOUND,
+                  samlResponse.getIssuer()
+              ));
 
-    Optional<Integration> samlProvider = providers.stream().filter(provider -> {
-      Optional<String> idpAliesOptional = SamlParameter.IDP_ALIAS.getParameter(provider);
-      return idpAliesOptional.isPresent() && idpAliesOptional.get()
-          .equalsIgnoreCase(samlResponse.getIssuer());
-    }).findFirst();
+          return createSamlUser(samlResponse.getAttributes(), provider);
+        });
+  }
 
-    String userName = checkUserName(CROP_DOMAIN.apply(samlResponse.getNameId().value()));
+  private User createSamlUser(Map<String, String> details, Integration integration) {
+    var email = resolveEmail(details, integration);
+    checkEmail(email);
 
     User user = new User();
-    user.setLogin(userName);
+    user.setLogin(email);
+    user.setEmail(email);
     user.setUuid(UUID.randomUUID());
+    user.setFullName(resolveName(details, integration));
     user.setActive(Boolean.TRUE);
-
-    if (samlProvider.isPresent()) {
-      populateUserDetailsIfSettingsArePresent(user, samlProvider.get(),
-          samlResponse.getAttributes());
-    } else {
-      populateUserDetails(user, samlResponse.getAttributes());
-    }
-
     user.setUserType(UserType.SAML);
+    user.setRole(resoleRole(details, integration));
     user.setExpired(false);
-
-    Project project = generatePersonalProject(user);
-    user.getProjects().add(project.getUsers().iterator().next());
-
     user.setMetadata(defaultMetaData());
+
+    var project = generatePersonalProject(user);
+
+    user.getProjects().add(project.getUsers().iterator().next());
 
     userRepository.save(user);
 
@@ -138,27 +163,44 @@ public class SamlUserReplicator extends AbstractUserReplicator {
     return user;
   }
 
-  private String checkUserName(String userName) {
-    String regex = "^" + userName + "(_[0-9]+)?$";
-    List<String> existingLogins = userRepository.findByLoginRegex(regex);
+  private String resolveEmail(Map<String, String> details, Integration integration) {
+    var attr = SamlParameter.EMAIL_ATTRIBUTE
+        .getParameter(integration)
+        .orElse(UserAttribute.EMAIL.toString());
 
-    if (existingLogins.isEmpty()) {
-      return userName;
+    return Optional.ofNullable(details.get(attr))
+        .filter(StringUtils::isNotBlank)
+        .map(NORMALIZE_STRING)
+        .orElseThrow(() -> new ReportPortalException(
+            ErrorType.ATTRIBUTE_NOT_FOUND,
+            attr
+        ));
+  }
+
+  private String resolveName(Map<String, String> details, Integration integration) {
+    Optional<String> fullNameAttr = SamlParameter.FULL_NAME_ATTRIBUTE.getParameter(integration);
+
+    if (fullNameAttr.isPresent()) {
+      return details.get(fullNameAttr.get());
     }
 
-    int maxPostfix = 0;
-    for (String login : existingLogins) {
-      if (login.equals(userName)) {
-        continue;
-      }
-      String suffix = login.substring(userName.length() + 1);
-      try {
-        int num = Integer.parseInt(suffix);
-        maxPostfix = Math.max(maxPostfix, num);
-      } catch (NumberFormatException ignored) {
-      }
-    }
-    return userName + "_" + (maxPostfix + 1);
+    var firstNameAttr = SamlParameter.FIRST_NAME_ATTRIBUTE.getParameter(integration)
+        .orElse(UserAttribute.FIRST_NAME.toString());
+    var lastNameAttr = SamlParameter.LAST_NAME_ATTRIBUTE.getParameter(integration)
+        .orElse(UserAttribute.LAST_NAME.toString());
+
+    return String.join(" ", details.get(firstNameAttr), details.get(lastNameAttr));
+  }
+
+  private UserRole resoleRole(Map<String, String> details, Integration integration) {
+    var attr = SamlParameter.ROLES_ATTRIBUTE
+        .getParameter(integration)
+        .orElse(UserAttribute.ROLES.toString());
+
+    return Optional.ofNullable(details.get(attr))
+        .filter(role -> role.toLowerCase().contains("admin"))
+        .map(role -> UserRole.ADMINISTRATOR)
+        .orElse(UserRole.USER);
   }
 
   private void publishActivityEvents(User user, Project project) {
@@ -181,47 +223,5 @@ public class SamlUserReplicator extends AbstractUserReplicator {
   private void publishUserAssignToProjectEvent(User user, Project project) {
     eventPublisher.publishEvent(
         new AssignUserEvent(user.getId(), user.getLogin(), project.getId()));
-  }
-
-  private void populateUserDetails(User user, Map<String, String> details) {
-    String email = NORMALIZE_STRING.apply(details.get(UserAttribute.EMAIL.toString()));
-    checkEmail(email);
-    user.setEmail(email);
-    String firstName = details.get(UserAttribute.FIRST_NAME.toString());
-    String lastName = details.get(UserAttribute.LAST_NAME.toString());
-    user.setFullName(String.join(" ", firstName, lastName));
-
-    user.setRole(UserRole.USER);
-  }
-
-  private void populateUserDetailsIfSettingsArePresent(User user, Integration integration,
-      Map<String, String> details) {
-
-    String email = NORMALIZE_STRING.apply(
-        details.get(SamlParameter.EMAIL_ATTRIBUTE.getParameter(integration).orElse(null)));
-    checkEmail(email);
-    user.setEmail(email);
-
-    Optional<String> idpFullNameOptional =
-        SamlParameter.FULL_NAME_ATTRIBUTE.getParameter(integration);
-
-    if (idpFullNameOptional.isEmpty()) {
-      String firstName = details.get(
-          SamlParameter.FIRST_NAME_ATTRIBUTE.getParameter(integration).orElse(null));
-      String lastName = details.get(
-          SamlParameter.LAST_NAME_ATTRIBUTE.getParameter(integration).orElse(null));
-      user.setFullName(String.join(" ", firstName, lastName));
-    } else {
-      String fullName = details.get(idpFullNameOptional.get());
-      user.setFullName(fullName);
-    }
-
-    String roles = details.get(
-        SamlParameter.ROLES_ATTRIBUTE.getParameter(integration).orElse(null));
-    if (Objects.nonNull(roles) && roles.toLowerCase().contains("admin")) {
-      user.setRole(UserRole.ADMINISTRATOR);
-    } else {
-      user.setRole(UserRole.USER);
-    }
   }
 }
