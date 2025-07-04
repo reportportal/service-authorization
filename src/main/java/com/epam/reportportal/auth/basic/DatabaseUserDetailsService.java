06eeebd7398beb67fc71bb857d17356629e2e425
raikbitters <raikbitters@gmail.com>
2025-07-04 11:58:55 +0200

    Remove unused import for UsernameNotFoundException in DatabaseUserDetailsService

--- src/main/java/com/epam/reportportal/auth/basic/DatabaseUserDetailsService.java
+++ src/main/java/com/epam/reportportal/auth/basic/DatabaseUserDetailsService.java
@@ -25,6 +25,7 @@ import com.epam.reportportal.auth.util.AuthUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
