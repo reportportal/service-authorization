98f4c456525fbe8bb827b44be80b7be946ae237d
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2026-08-27 16:18:04 +0300

    EPMRPP-118917 || Enhance XML parsing security in SamlResponseParser (#440)

--- src/main/java/com/epam/reportportal/auth/integration/saml/SamlResponseParser.java
+++ src/main/java/com/epam/reportportal/auth/integration/saml/SamlResponseParser.java
@@ -47,6 +47,13 @@ public class SamlResponseParser {
 
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     factory.setNamespaceAware(true);
+    factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+    factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+    factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+    factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+    factory.setXIncludeAware(false);
+    factory.setExpandEntityReferences(false);
+
     DocumentBuilder builder = factory.newDocumentBuilder();
     Document document = builder.parse(new ByteArrayInputStream(samlResponse.getBytes()));
 
